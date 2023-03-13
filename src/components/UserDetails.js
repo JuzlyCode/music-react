bd351e1105d2bdc9ce82f0dae05138f3ec109d3f
JuzlyCode <116904449+JuzlyCode@users.noreply.github.com>
2023-03-13 20:54:44 +0700

    Update UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -6,7 +6,7 @@ export default function UserDetails() {
 	const [userData, setUserData] = useState('');
 
 	useEffect(() => {
-		fetch('https://178.128.104.249:5000/userData', {
+		fetch('http://178.128.104.249:5000/userData', {
 			method: 'POST',
 			crossDomain: true,
 			headers: {
