e1d56d4b12b0909b0d08ceb43d3dc46461b8866e
JuzlyCode <116904449+JuzlyCode@users.noreply.github.com>
2023-03-14 18:19:53 +0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ export default function Login() {
 		e.preventDefault();
 		console.log(email, password);
 		window.localStorage.setItem('email', email);
-		fetch('https://178.128.104.249:5000/login-user', {
+		fetch('hhttps://music-node-js.vercel.app/login-user', {
 			method: 'POST',
 			crossDomain: true,
 			headers: {
@@ -56,7 +56,7 @@ export default function Login() {
 			e.preventDefault();
 
 			console.log(email2, password2, checkPassword2);
-			fetch('https://178.128.104.249:5000/register', {
+			fetch('https://music-node-js.vercel.app/register', {
 				method: 'POST',
 				crossDomain: true,
 				headers: {
