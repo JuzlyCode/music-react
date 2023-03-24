c9a960d5905f4ba6dbc8aaee9f439ad8bb1e6194
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2023-03-25 02:34:52 +0700

    fix open bar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -21,9 +21,7 @@ export default function Navbar() {
             <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
               <Link to={"/"}>
                 <li>
-                  <div className="hover:text-gray-200">
-                    Home
-                  </div>
+                  <div className="hover:text-gray-200">Home</div>
                 </li>
               </Link>
               <li>
@@ -44,10 +42,7 @@ export default function Navbar() {
               {/* Logout */}
               {currentUser && (
                 <>
-                  <Link
-                    to={"/my-playlist"}
-                    className="hover:text-gray-200"
-                  >
+                  <Link to={"/my-playlist"} className="hover:text-gray-200">
                     <button className="flex gap-x-2 items-center">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -105,44 +100,50 @@ export default function Navbar() {
           </button>
         </nav>
       </section>
-      <div className="xl:hidden fixed top-20 -mt-2 bg-slate-800 w-full text-white border-b">
-        {!currentUser && <Authenticate padding={true} />}
-        {/* Logout */}
-        {currentUser && (
-          <>
-            <Link to={"/my-playlist"} className="hover:text-gray-200" href="#">
-              <button className="flex gap-x-2 p-5 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-                <div>My playlist</div>
-              </button>
-            </Link>
-            <div className="p-5">{currentUser.email}</div>
+      {openBar && (
+        <div className="xl:hidden fixed top-20 -mt-2 bg-slate-800 w-full text-white border-b">
+          {!currentUser && <Authenticate padding={true} />}
+          {/* Logout */}
+          {currentUser && (
+            <>
+              <Link
+                to={"/my-playlist"}
+                className="hover:text-gray-200"
+                href="#"
+              >
+                <button className="flex gap-x-2 p-5 items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                  <div>My playlist</div>
+                </button>
+              </Link>
+              <div className="p-5">{currentUser.email}</div>
 
-            <button
-              onClick={() => {
-                setCurrentUser();
-                localStorage.removeItem("token");
-              }}
-              className="text-white p-5"
-            >
-              Logout
-            </button>
-          </>
-        )}
-      </div>
+              <button
+                onClick={() => {
+                  setCurrentUser();
+                  localStorage.removeItem("token");
+                }}
+                className="text-white p-5"
+              >
+                Logout
+              </button>
+            </>
+          )}
+        </div>
+      )}
     </>
   );
 }
